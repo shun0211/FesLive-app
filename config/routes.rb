205e50c0fd58c2996216c8d14ae89350d01d3e7d
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-05-10 20:05:52 +0900

    新規登録画面完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   
   resources :tickets, only: [:index]
 
