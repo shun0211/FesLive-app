46f9846475c898da4cad70264d51a9bb47ce6300
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-06-06 09:34:46 +0900

    20.6.6

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,14 @@
 class LikesController < ApplicationController
   def create
     @like = Like.create(set_list_id: params[:set_list_id], user_id: current_user.id)
+    @set_list = SetList.find(params[:set_list_id])
+    @event = Event.find(params[:event_id])
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, set_list_id: params[:set_list_id])
+    @set_list = SetList.find(params[:set_list_id])
+    @event = Event.find(params[:event_id])
     @like.destroy
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   has_many :set_lists, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: "を○○@○○.○○の形式で入力して下さい" }, length: { maximum: 255 }
+  validates :nickname, presence: true, length: { in: 1..10 }
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*\d)[a-z\d]{8,}+\z/
+  validates :password, presence: true, length: { maximum: 75 }, format: { with: VALID_PASSWORD_REGEX, message: "を半角英数字8文字以上で入力して下さい" }
 end
